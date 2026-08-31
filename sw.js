c0d51814bbb99bb4dc21baddd1c79d582b758bb1
akshneerSingh <akshneer005@icloud.com>
2026-08-31 15:22:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
    Ohne Netz ändert sich nichts: dann greift der Zwischenspeicher wie bisher.
    ========================================================================== */
 
-const VERSION = "v39";
+const VERSION = "v40";
 const CACHE = "pruefungen-" + VERSION;
 
 const SHELL = [
